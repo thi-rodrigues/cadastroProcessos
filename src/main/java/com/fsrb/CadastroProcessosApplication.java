8e362d904180a38ee39a5db1b3b17c37d14eb42e
thi-rodrigues <thiagorodrigues1203@outlook.com>
2024-06-01 15:52:28 -0300

    projeto iniciado

---
+++ src/main/java/com/fsrb/CadastroProcessosApplication.java
@@ -0,0 +1,13 @@
+package com.fsrb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroProcessosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroProcessosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsrb/CadastroProcessosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsrb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroProcessosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
